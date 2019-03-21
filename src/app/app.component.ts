bb0ae93f1301701c087bdb1eea01e8a877795645
Mandar143 <mmkirad@gmail.com>
2019-03-21 23:10:34 +0530

    questions quiz

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,23 +1,50 @@
 import { Component, OnInit } from '@angular/core';
 import { QuestionServiceService } from './question-service.service';
-var a=require("./example_2.json");
+var a = require("./example_2.json");
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  providers:[QuestionServiceService]
+  providers: [QuestionServiceService]
 })
-export class AppComponent implements OnInit  {
+export class AppComponent implements OnInit {
   title = 'questions-angular';
-  
+  typeOfQuiz: any;
+  stQuiz:Boolean=false;
+  type:Boolean=true;
+  Questions:any;
+  options: any;
   constructor(
-    private appSettingsService : QuestionServiceService 
-) { 
-  console.log(a);
-  
-}
+    private appSettingsService: QuestionServiceService
+  ) {
+    console.log(a.quiz);
+    this.typeOfQuiz = Object.keys(a.quiz);
+  }
 
-ngOnInit(){
- 
-}   
+  ngOnInit() {
+
+  }
+  nextQuiz(value){
+    console.log(value);
+    
+  }
+  startQuiz(value){
+    console.log(value);
+    if(value){
+      this.stQuiz=true;
+      this.type=false;
+      this.typeOfQuiz.filter(type=>{
+        if(type===value){
+          this.Questions=Object.values(a.quiz[type]);
+          console.log(this.Questions);
+          this.Questions.filter(options=>{
+            console.log(options.options);
+            this.options=options.options;
+          })
+        }
+        
+      });
+      //this.questions = Object.keys(a.quiz);
+    }
+  }
 }
